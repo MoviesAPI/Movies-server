bbb83a7fcecdcd7a14d9d62017a4faee8dcbdbdb
davidyunus <dove.fit@gmail.com>
2018-07-13 13:43:12 +0700

    api NYT

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 const User = require('../models/user')
-const bcrypt = require('bcrypt')
-const saltRound = 10
-const ObjectID = require('mongodb').ObjectID
+const axios = require('axios')
 
 class UserController {
     
@@ -79,6 +77,21 @@ class UserController {
                     .json(err)
             })
     }
+
+    static getSearch(req, res) { 
+        // console.log('something', req.body.search)
+            var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+            axios.get(url, {
+                headers: {
+                    'api-key': "cf9b4f4cd0454fd3b15ac584bcc5dd35",
+                    'query': req.body.search
+                }
+            })
+            .then(function(response) {
+                console.log('THIS IS RESPONSE', response.data)
+                res.json(response.data)
+            })
+        }
 }
 
 module.exports = UserController
